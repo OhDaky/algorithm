c8cc3cd225451bc74f17abd98bf2925f2deeadcd
OhDaky <ttakko88@gmail.com>
2023-02-24 14:25:13 +0900

    inflearn-01-1

---
+++ 0108.js
@@ -0,0 +1,11 @@
+function solution(arr) {
+  let answer = [];
+  let sum = arr.reduce((acc, cur) => acc + cur, 0) - 100;
+  let rest = arr.length - 7;
+
+  // rest 수 만큼 합쳤을 때 sum이 나오게
+
+  return answer;
+}
+
+// let arr=[20, 7, 23, 19, 10, 15, 25, 8, 13];
