1d5bb969c0230b6da2c86781fc505e5bd5604ca4
Dakyeong Oh <50348901+OhDaky@users.noreply.github.com>
2023-03-23 10:14:47 +0900

    programmers_43165 타겟넘버 dfs

---
+++ q-43165.js
@@ -0,0 +1,31 @@
+function solution(numbers, target) {
+    var answer = 0;
+    function dfs(depth, sum) {
+        if(depth === numbers.length) {
+            if(sum === target) {
+                answer += 1;
+            }
+            return;
+        }
+        dfs(depth+1, sum + numbers[depth])
+        dfs(depth+1, sum - numbers[depth])
+    }
+    
+    dfs(0,0)
+    console.log(answr)
+    return answer;
+}
+
+// dfs
+
+
+const numbers1 = [1, 1, 1, 1, 1];
+const target1 = 3;
+// expected 5
+
+const numbers2 = [4, 1, 2, 1];
+const target2 = 4;
+// expected 2
+
+solution(numbers1, target1);
+solution(numbers2, target2);
