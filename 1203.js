7cf43654075644237102c81ba43fc7fa9dd3a494
OhDaky <ttakko88@gmail.com>
2023-04-10 14:19:45 +0900

    이코테 12 - 문자열 압축

--- 1203.js
+++ 1203.js
@@ -1,6 +1,23 @@
 function solution(s) {
-  let answer = 0;
+  let answer = s.length;
 
+  for (let i = 1; i <= Math.floor(s.length / 2); i++) {
+    let compressed = "";
+    let prev = s.slice(0, i);
+    let cnt = 1;
+
+    for (let j = i; j < s.length; j += i) {
+      if (prev === s.slice(j, j + i)) {
+        cnt++;
+      } else {
+        compressed += cnt >= 2 ? cnt + prev : prev;
+        prev = s.slice(j, j + i);
+        cnt = 1;
+      }
+    }
+    compressed += cnt >= 2 ? cnt + prev : prev;
+    answer = Math.min(answer, compressed.length);
+  }
   console.log(answer);
   return answer;
 }
