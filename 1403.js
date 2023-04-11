fc68d31a8adf8c99a46e4a8657d232b328d26e01
OhDaky <ttakko88@gmail.com>
2023-04-11 18:58:16 +0900

    wip : 이코테 14 - 실패율

---
+++ 1403.js
@@ -0,0 +1,12 @@
+function solution(n, arr) {
+  let res = [];
+  // 전체 스테이지 n
+  // 실패율 = 스테이지에 도달. 클리어 하지 못함 / 스테이지 도달
+  // 실패율 내림차순 스테이지 return
+
+  const stages = arr.length;
+
+  return res;
+}
+
+console.log(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]));
