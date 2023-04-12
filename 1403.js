506d3de3a71860c39457b31f2417683ae6a26c91
OhDaky <ttakko88@gmail.com>
2023-04-12 20:16:39 +0900

    이코테 14 - 실패율, programmers_42889

--- 1403.js
+++ 1403.js
@@ -1,12 +1,22 @@
 function solution(n, arr) {
-  let res = [];
-  // 전체 스테이지 n
-  // 실패율 = 스테이지에 도달. 클리어 하지 못함 / 스테이지 도달
-  // 실패율 내림차순 스테이지 return
+  let res = new Array(n + 2).fill().map(() => new Set());
+  let fails = [];
+  let members = arr.length;
+  for (let i = 0; i <= n + 1; i++) {
+    for (let j = 0; j < members; j++) {
+      if (arr[j] === i) {
+        res[i].add(j);
+      }
+    }
+  }
 
-  const stages = arr.length;
-
-  return res;
+  for (let i = 1; i < n + 1; i++) {
+    fails.push([i, res[i].size / members]);
+    members -= res[i].size;
+  }
+  fails.sort((a, b) => b[1] - a[1]);
+  fails = fails.map((el) => el[0]);
+  return fails;
 }
 
 console.log(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]));
