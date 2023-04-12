7e310d93ee594e286b476cb34ed1bfdc63021f41
OhDaky <ttakko88@gmail.com>
2023-04-12 20:57:25 +0900

    이코테 14 - 카드 정렬하기

---
+++ 1404.js
@@ -0,0 +1,30 @@
+function solution(N, arr) {
+  let res = 0;
+  let heap = [];
+  for (let i of arr) {
+    heap.push([i, [...arr]]);
+  }
+
+  while (heap.length !== 1) {
+    let one = heap[0][0];
+    let two = heap[1][0];
+    let sum = one + two;
+    heap = heap.slice(2);
+
+    for (let i = 0; i < heap.length; i++) {
+      heap[i][1] = [sum, ...arr.filter((el) => el !== one && el !== two)];
+    }
+    res += sum;
+  }
+
+  return res + heap[0][0];
+}
+
+console.log(solution(3, [10, 20, 40]));
+
+/*
+10 20
+30 40  = 100
+
+10 20 30 50 80 90
+*/
