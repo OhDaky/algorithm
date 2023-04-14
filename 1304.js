f3666be956e7496787892fcd108fcfa49915cbca
OhDaky <ttakko88@gmail.com>
2023-04-14 15:31:50 +0900

    이코테 13 - 괄호 변환

--- 1304.js
+++ 1304.js
@@ -1,73 +1,62 @@
-function isRight(arr) {
+function isBalanced(p) {
   let cnt = 0;
-  for (let i of arr) {
-    if (i === "(") cnt++;
+  for (let i = 0; i < p.length; i++) {
+    if (p[i] === "(") cnt++;
     else cnt--;
-
-    if (cnt < 0) return false;
+    if (cnt === 0) return i;
   }
-  return cnt === 0 ? true : false;
 }
 
-function solution(str) {
-  let res = [];
-  const arr = str.split("");
-  let cntStart = 0;
-  let cntEnd = 0;
-  let u = [];
-  let v = [];
-
-  function DFS(arr) {
-    /* u 올바르지 않으면
-     * 1. 빈문자열에 첫번째 문자 ( 추가
-     * 2. v에 대해 이어붙임
-     * 3. ) 추가
-     * 4. u 첫번쨰와 마지막 제거, 나머지 문자열의 괄호 방향을 뒤집어서 뒤에서 붙임
-     * 5. 반환
-     */
-
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] === "(") cntStart++;
-      else cntEnd++;
-
-      if (cntStart === cntEnd) {
-        u = arr.slice(0, i + 1);
-        v = arr.slice(i + 1, arr.length + 1);
-        i = arr.length;
-      }
+function checkProper(p) {
+  let cnt = 0;
+  for (let i of p) {
+    if (i === "(") cnt++;
+    else {
+      if (cnt === 0) return false;
+      cnt--;
     }
-
-    if (isRight(u) && isRight(v)) res = [...res, ...u, ...v];
-    if (isRight(u) && !isRight(v)) res = [...res, ...u];
-
-    if (!isRight(u)) {
-      let newArr = ["(", ...v, ")"];
-      let newU = u.slice(1, u.length - 1);
-      for (let i of newU) {
-        i === "(" ? (i = ")") : (i = "(");
-      }
-      newArr = [...newArr, ...newU];
-      DFS(newArr);
-    }
-
-    if (!isRight(v)) {
-      DFS(v);
-    }
-    return u.join("").concat(v.join(""));
   }
-
-  DFS(arr);
-  console.log(res.join(""));
-  return res.join("");
+  return true;
 }
+
+function solution(p) {
+  let res = "";
+  if (p === "") return res;
+  let index = isBalanced(p);
+  let u = p.slice(0, index + 1);
+  let v = p.slice(index + 1);
+
+  if (checkProper(u)) {
+    res = u + solution(v);
+  } else {
+    res = "(";
+    res += solution(v);
+    res += ")";
+    u = u.slice(1, u.length - 1);
+    let newU = "";
+    for (let i = 0; i < u.length; i++) {
+      if (u[i] === "(") newU += ")";
+      else newU += "(";
+    }
+    res += newU;
+  }
+  return res;
+}
+/* u 올바르지 않으면
+ * 1. 빈문자열에 첫번째 문자 ( 추가
+ * 2. v에 대해 이어붙임
+ * 3. ) 추가
+ * 4. u 첫번쨰와 마지막 제거, 나머지 문자열의 괄호 방향을 뒤집어서 뒤에서 붙임
+ * 5. 반환
+ */
 
 const case1 = "(()())()";
 const case2 = ")(";
 const case3 = "()))((()";
 
-solution(case1);
-solution(case2);
-solution(case3);
+console.log(solution(case1));
+console.log(solution(case2));
+console.log(solution(case3));
 
 /**
  * 개수 맞을 때
