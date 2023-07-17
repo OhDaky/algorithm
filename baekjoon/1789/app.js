3507141b7a6d9470030815791aa39eb2c513f38d
OhDaky <ttakko88@gmail.com>
2023-07-17 16:37:02 +0900

    백준 1789. 수들의 합 - greedy

---
+++ baekjoon/1789/app.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const filePath = __dirname + "/input.txt";
+//   process.platform === linux? /dev/stdin
+const S = fs.readFileSync(filePath).toString();
+let sum = 0,
+  cnt = 0;
+
+for (let i = 1; i < S; i++) {
+  sum += i;
+  cnt++;
+  if (sum >= S) {
+    cnt--;
+    i = S;
+  }
+}
+
+console.log(cnt);
