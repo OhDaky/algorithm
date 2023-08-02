cefe9e1d81e452f4a3e57ee42d30bdff6d22db88
OhDaky <ttakko88@gmail.com>
2023-08-02 15:09:43 +0900

    백준 2164. 카드2 - 자료구조

---
+++ baekjoon/2164/app.js
@@ -0,0 +1,60 @@
+const fs = require("fs");
+const filePath = __dirname + "/input.txt";
+//   process.platform === linux? "/dev/stdin"
+const input = +fs.readFileSync(filePath).toString().trim();
+let queue = Array.from({ length: input }, (_, num) => num + 1);
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) this.head = newNode;
+    else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+    }
+    this.tail = newNode;
+    this.length++;
+    return newNode;
+  }
+
+  getHead() {
+    return this.head.value;
+  }
+
+  removeHead() {
+    this.head = this.head.next;
+    this.head.prev = null;
+    this.length--;
+  }
+
+  getSize() {
+    return this.length;
+  }
+}
+
+const list = new LinkedList();
+
+for (let i = 1; i <= input; i++) list.push(i);
+
+while (list.length > 1) {
+  list.removeHead();
+  list.push(list.getHead());
+  list.removeHead();
+}
+
+console.log(list.getHead());
